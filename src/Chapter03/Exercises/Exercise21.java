be1639b7bdc33523bdb3db0d570bf716e3c050ba
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-26 23:33:20 -0800

    Complete Chapter 3 Exercise 21

--- src/Chapter03/Exercises/Exercise21.java
+++ src/Chapter03/Exercises/Exercise21.java
@@ -1,6 +1,18 @@
 package Chapter03.Exercises;
 
+import java.util.*;
+
 public class Exercise21 {
+    public static void main(String[] args) {
+        processName(new Scanner(System.in));
+    }
+
+    public static void processName(Scanner input) {
+        System.out.print("Please enter your full name: ");
+        String firstName = input.next();
+        String lastName = input.next();
+        System.out.print("Your name in reverse order is " + lastName + ", " + firstName);
+    }
 }
 /*
 Write a method called processName that accepts a Scanner for the console as a parameter and prompts the user to enter a full name,
