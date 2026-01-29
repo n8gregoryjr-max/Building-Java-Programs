dcafab165d7a8390db77c05e2128fcd7c9d4ba3c
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 21:40:17 -0800

    Complete Chapter 3 Exercise 7

--- src/Chapter03/Exercises/Exercise07.java
+++ src/Chapter03/Exercises/Exercise07.java
@@ -1,6 +1,17 @@
 package Chapter03.Exercises;
 
 public class Exercise07 {
+    public static void main(String[] args) {
+        largestAbsVal(7, -2, -11);
+    }
+
+    public static void largestAbsVal(int one, int two, int three){
+        one = Math.abs(one);
+        two = Math.abs(two);
+        three = Math.abs(three);
+        int answer = Math.max(Math.max(one, two), three);
+        System.out.print(answer);
+    }
 }
 /*
 Write a variation of the largestAbsVal method from the last exercise that takes three integers as parameters and returns the largest of their three absolute values.
