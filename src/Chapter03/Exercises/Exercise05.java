b60a65ed776b2732b53e9c6191835f216b327a3d
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 21:28:09 -0800

    Complete Chapter 3 Exercise 5

--- src/Chapter03/Exercises/Exercise05.java
+++ src/Chapter03/Exercises/Exercise05.java
@@ -1,6 +1,18 @@
 package Chapter03.Exercises;
 
 public class Exercise05 {
+    public static void main(String[] args) {
+        printGrid(4, 6);
+    }
+
+    public static void printGrid(int rows, int columns){
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print((j * 4) + i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
 /*
 Write a method called printGrid that accepts two integers representing a number of rows and columns and prints a grid of integers from 1 to (rows * columns) in column major order.
