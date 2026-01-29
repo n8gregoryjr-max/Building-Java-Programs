67f5b9a1bfb701bd41db8f9f2359a9fa317959cc
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 23:11:31 -0800

    Complete Chapter 3 Exercise 18

--- src/Chapter03/Exercises/Exercise18.java
+++ src/Chapter03/Exercises/Exercise18.java
@@ -1,6 +1,16 @@
 package Chapter03.Exercises;
 
 public class Exercise18 {
+    public static void main(String[] args) {
+        vertical("hey now");
+    }
+
+    public static void vertical(String word) {
+            // Prints one letter at a time for the length of the String.
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
 }
 /*
 Write a method called vertical that accepts a string as its parameter and prints each letter of the string on separate lines.
@@ -9,6 +19,7 @@ For example, a call of vertical("hey now") should produce the following output:
     h
     e
     y
+
     n
     o
     w
