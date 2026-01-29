bbbc8e2642e5faca4fb08773048ece8f02b97521
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 22:29:41 -0800

    Complete Chapter 3 Exercise 10

--- src/Chapter03/Exercises/Exercise10.java
+++ src/Chapter03/Exercises/Exercise10.java
@@ -1,6 +1,14 @@
 package Chapter03.Exercises;
 
 public class Exercise10 {
+    public static void main(String[] args) {
+        area(2);
+    }
+
+    public static void area(double r){
+            // The formula for the area of a circle.
+        System.out.print(r * r * Math.PI);
+    }
 }
 /*
 Write a method called area that accepts as a parameter the radius of a circle and that returns the area of the circle.
