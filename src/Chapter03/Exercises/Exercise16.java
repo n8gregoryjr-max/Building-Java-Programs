a33de222d0706749dc8dd863a2f71f39103cef99
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 23:01:01 -0800

    Complete Chapter 3 Exercise 16

--- src/Chapter03/Exercises/Exercise16.java
+++ src/Chapter03/Exercises/Exercise16.java
@@ -1,6 +1,15 @@
 package Chapter03.Exercises;
 
 public class Exercise16 {
+    public static void main(String[] args) {
+        triangleArea(8, 5.2, 7.1);
+    }
+
+    public static void triangleArea(double a, double b, double c) {
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+        System.out.print(area);
+    }
 }
 /*
 Write a method called triangleArea that accepts the three side lengths of a triangle as parameters and returns the area of a triangle with those side lengths.
@@ -8,6 +17,6 @@ For example, the call triangleArea(8, 5.2, 7.1) should return 18.151176098258745
 To compute the area, use Heron’s formula, which states that the area of a triangle whose three sides have lengths 𝑎, 𝑏, and 𝑐, is the following.
 The formula is based on the computed value 𝑠, a length equal to half the perimeter of the triangle:
 
-    Area = sqrt[𝑠(𝑠 - 𝑎)(𝑠 - 𝑏)(𝑠 - 𝑐)]
+    Area = sqrt(𝑠(𝑠 - 𝑎)(𝑠 - 𝑏)(𝑠 - 𝑐))
     𝑠 = (𝑎 + 𝑏 + 𝑐) / 2
  */
