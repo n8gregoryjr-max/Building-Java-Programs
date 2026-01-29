892db89cf24c85ac602c9ca1b4d06ef409a3acf5
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 23:35:56 -0800

    Complete Chapter 3 Exercise 20

--- src/Chapter03/Exercises/Exercise20.java
+++ src/Chapter03/Exercises/Exercise20.java
@@ -1,6 +1,27 @@
 package Chapter03.Exercises;
+    // Import the utility package for Scanner class.
+import java.util.*;
 
 public class Exercise20 {
+    public static void main(String[] args) {
+        inputBirthday();
+    }
+
+    public static void inputBirthday(){
+            // Initialize scanner, name it 'input'.
+        Scanner input = new Scanner(System.in);
+            // Ask question and store answer as a String.
+        System.out.println("On what day of the month were you born?");
+        String day = input.nextLine();
+            // Ask another question and store answer as a String.
+        System.out.println("What is the name of the month in which you were born?");
+        String month = input.nextLine();
+            // Ask last question and store answer as a String.
+        System.out.println("During what year were you born?");
+        String year = input.nextLine();
+            // Use stored strings to give a response based on answers.
+        System.out.println("You were born on " + month + " " + day + ", " + year +". You’re mighty old!");
+    }
 }
 /*
 Write a method called inputBirthday that accepts a Scanner for the console as a parameter and prompts the user
