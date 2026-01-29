60a5eddf7e0baad2ec62424294ebbd69dd335f02
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 23:16:54 -0800

    Complete Chapter 3 Exercise 19

--- src/Chapter03/Exercises/Exercise19.java
+++ src/Chapter03/Exercises/Exercise19.java
@@ -1,6 +1,17 @@
 package Chapter03.Exercises;
 
 public class Exercise19 {
+    public static void main(String[] args) {
+        printReverse("hello there!");
+    }
+
+    public static void printReverse(String word) {
+            // Prints string backwards by indexing from length down to 0.
+        for (int i = 1; i <= word.length(); i++) {
+            int reverse = word.length() - i;
+            System.out.print(word.charAt(reverse));
+        }
+    }
 }
 /*
 Write a method called printReverse that accepts a string as its parameter and prints the characters in opposite order.
