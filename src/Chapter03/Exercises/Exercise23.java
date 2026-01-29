d9ace404137141b8c0d0548d892cf94c8593b92c
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 20:10:39 -0800

    Complete Chapter 3 Exercise 23

--- src/Chapter03/Exercises/Exercise23.java
+++ src/Chapter03/Exercises/Exercise23.java
@@ -1,6 +1,18 @@
 package Chapter03.Exercises;
 
 public class Exercise23 {
+    public static void main(String[] args) {
+        printIndexed("ZELDA");
+    }
+
+    public static void printIndexed(String zelda){
+        int stringLength = zelda.length();
+        for (int i = 0; i < stringLength; i++) {
+            char letter = zelda.charAt(i);
+            System.out.print(letter);
+            System.out.print(stringLength - 1 - i);
+        }
+    }
 }
 /*
 Write a method called printIndexed that accepts a string as its parameter and prints the string’s characters in order followed by their indexes in reverse order.
