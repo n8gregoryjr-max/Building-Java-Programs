5e8eeb569f4a1fe69f0a71098e6bf67503630c1a
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-01-28 22:24:14 -0800

    Complete Chapter 3 Exercise 9

--- src/Chapter03/Exercises/Exercise09.java
+++ src/Chapter03/Exercises/Exercise09.java
@@ -1,7 +1,17 @@
 package Chapter03.Exercises;
 
 public class Exercise09 {
+    public static void main(String[] args) {
+        lastDigit(-12345);
+    }
+
+    public static void lastDigit(int num){
+            /* Instead of converting it into a String and getting the last value of the String, I wanted to
+                    figure out the math behind it, so I used StackOverflow (Link below) */
+        System.out.print(Math.abs(num % 10));
+    }
 }
+// https://stackoverflow.com/questions/17144997/gets-last-digit-of-a-number
 /*
 Write a method called 'lastDigit' that returns the last digit of an integer. For example, 'lastDigit(3572)' should return 2.
 It should work for negative numbers as well. For example, 'lastDigit(-947)' should return 7.
