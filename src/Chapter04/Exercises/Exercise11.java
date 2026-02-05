b611a34d36c3852152c28ac6ed2b331263178fb9
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-04 22:24:23 -0800

    Complete Chapter 4 Exercise 11

--- src/Chapter04/Exercises/Exercise11.java
+++ src/Chapter04/Exercises/Exercise11.java
@@ -1,6 +1,47 @@
 package Chapter04.Exercises;
 
+import java.util.*;
+
 public class Exercise11 {
+    void main() {
+        Scanner console = new Scanner(System.in);
+        longestName(console, 4);
+    }
+
+    public static void longestName(Scanner input, int n) {
+            // Creates a placeholder for the longest name.
+        String winner = "";
+
+        for (int i = 1; i <= n; i++) {
+                // Prompts user to enter a name.
+            IO.print("name #" + i + "? ");
+            String name = input.nextLine();
+
+                // Makes placeholder into the longest name.
+            if (name.length() > winner.length()) {
+                    winner = name;
+            } else if (name.isEmpty()) {
+
+                    // Error checker, if nothing was entered.
+                IO.print("Error! No name entered.");
+
+                    // Ends for loop and set String winner to trigger ERROR message.
+                i = n + 1;
+                winner = "ERROR";
+            }
+        }
+
+            // Error catcher.
+        if (winner .equals("ERROR")) {
+            IO.println("\nPlease try again!");
+
+        } else {
+                // Format name before sending last message.
+            winner = winner.substring(0, 1).toUpperCase() + winner.substring(1).toLowerCase();
+
+            IO.print(winner + "'s name is longest");
+        }
+    }
 }
 /*
 Write a method called 'longestName' that accepts a Scanner for the console and an integer n as parameters
