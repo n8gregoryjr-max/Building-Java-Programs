e35b9a6e4ac37607c5516bbfd996172af5d06450
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-04 23:42:51 -0800

    Complete Chapter 4 Exercise 18

--- src/Chapter04/Exercises/Exercise18.java
+++ src/Chapter04/Exercises/Exercise18.java
@@ -1,6 +1,41 @@
 package Chapter04.Exercises;
 
 public class Exercise18 {
+    void main() {
+        IO.print(wordCount(" hello "));
+    }
+
+    public static int wordCount(String words) {
+            // Pre-checks for Errors before running code.
+        if (words == null) {
+            throw new IllegalArgumentException();
+        }
+
+            // Holds value equal to number of words.
+        int wordCount = 0;
+
+            // This is a toggle to see if we are in a word.
+        boolean word = false;
+
+            // Looks at every character in order.
+        for (int i = 0; i < words.length(); i++) {
+            char c = words.charAt(i);
+
+                // If the character is not a space and starts a new word, toggle true and add 1 to word counter.
+            if (c != ' ') {
+                if (!word) {
+                    wordCount++;
+                    word = true;
+                }
+
+                // If the character is a space, then toggle off to either repeat cycle or return word counter.
+            } else {
+                word = false;
+            }
+        }
+
+        return wordCount;
+    }
 }
 /*
 Write a method called 'wordCount' that accepts a String as its parameter and returns the number of words in the String.
