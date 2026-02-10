848c1008855c68b1c5f7e7b4e1a7a7d1dc6019c4
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-09 20:47:26 -0800

    Complete Chapter 5 Project 1

--- src/Chapter05/Projects/Project01.java
+++ src/Chapter05/Projects/Project01.java
@@ -1,6 +1,52 @@
 package Chapter05.Projects;
 
+import java.util.Scanner;
+
 public class Project01 {
+    void main() {
+        pigLatin(new Scanner(System.in));   // Initializing scanner.
+    }
+
+    public static void pigLatin(Scanner input) {
+            // Initializing final answer.
+        String result = "";
+
+            // Introduction and prompts user for a sentence.
+        IO.print("Thank you for using this English to Pig Latin tool!\n" +
+                "Enter a sentence: ");
+        String sentence = input.nextLine();
+
+            // Breaks scanner into bite sized pieces.
+        Scanner words = new Scanner(sentence);
+
+            // Loops the translation for each word and adds it to the placeholder String.
+        while (words.hasNext()) {
+            String word = words.next();
+            result += translation(word) + " ";
+        }
+
+            // Outputs final result, minus any spaces before or after.
+        System.out.println(result.trim());
+    }
+
+    public static String translation(String word) {
+        char first = word.charAt(0);
+            // Check for one-letter words.
+        if (word.length() != 1 && first != 'a' && first != 'A' && first != 'e' &&
+                first != 'E' && first != 'i' && first != 'I' && first != 'o' &&
+                first != 'O' && first != 'u' && first != 'U') {
+                // Grabs second character.
+            char second = word.charAt(1);
+                // Checks if it is a Th, Wh, Bl, or any of those.
+            if (second == 'h' || second == 'l') {
+                return (word.substring(2) + "-" + first + second + "ay");
+
+            } else {
+                return (word.substring(1) + "-" + first + "ay");
+            }
+        }
+        return (word + "-ay");
+    }
 }
 /*
 Write an interactive program that reads lines of input from the user and converts each line into “Pig Latin.”
