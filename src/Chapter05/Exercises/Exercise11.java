9eab6f4cfe01ba232d74a76fc308d194075d5f9e
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-09 17:55:53 -0800

    Complete Chapter 5 Exercise 11

--- src/Chapter05/Exercises/Exercise11.java
+++ src/Chapter05/Exercises/Exercise11.java
@@ -1,6 +1,35 @@
 package Chapter05.Exercises;
 
+import java.util.Random;
+
 public class Exercise11 {
+    void main() {
+        threeHeads(54321);
+    }
+    public static void threeHeads(int seed) {
+            // Generate Pseudorandom number generator w/ set seed.
+        Random r = new Random(seed);
+
+            // for loop that counts up to 3, signifying 3 heads.
+        for (int i = 0; i < 3;) {
+
+                // Set placeholder number
+            int a = r.nextInt(2);
+
+                // Incrementally increase 'i' per head rolled.
+            if (a == 1) {
+                IO.print("H ");
+                i++;
+
+                // Reset counter every tails rolled.
+            } else {
+                IO.print("T ");
+                i = 0;
+            }
+        }
+            // VICTORY!!!
+        IO.print("\nThree heads in a row!");
+    }
 }
 /*
 Write a method called 'threeHeads' that repeatedly flips a coin until the results of the coin toss are three heads in a row.
