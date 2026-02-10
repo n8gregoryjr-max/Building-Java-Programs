49ba0c2f0e9e4baf06506db9ec04346c703e294b
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-09 18:49:34 -0800

    Complete Chapter 5 Exercise 18

--- src/Chapter05/Exercises/Exercise18.java
+++ src/Chapter05/Exercises/Exercise18.java
@@ -1,6 +1,30 @@
 package Chapter05.Exercises;
 
 public class Exercise18 {
+    void main() {
+        int a = digitSum(0);
+        IO.print(a);
+    }
+
+    public static int digitSum(int num) {
+            // Initialize final value.
+        int sum = 0;
+
+            // Change input to a positive number.
+        num = Math.abs(num);
+
+            // Loop for as many digits are in a number.
+        while (num != 0) {
+
+                // Add digit to sum for each digit there is.
+            sum += (num % 10);
+
+                // Move onto the next digit.
+            num = num / 10;
+        }
+            // Print sum of all digits.
+        return sum;
+    }
 }
 /*
 Write a method called 'digitSum' that accepts an integer as a parameter and returns the sum of the digits of that number.
