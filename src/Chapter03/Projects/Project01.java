fcc58b4385916beb6238fda711c58b63e6859747
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-14 22:33:25 -0800

    Complete Chapter 3 Project 1

--- src/Chapter03/Projects/Project01.java
+++ src/Chapter03/Projects/Project01.java
@@ -1,6 +1,50 @@
 package Chapter03.Projects;
 
 public class Project01 {
+    void main() {
+        christmasTrees(4, 7);
+    }
+    
+    public static void christmasTrees(int segment, int height) {
+        // formulas used for scaling in all loops.
+        int space = height + segment - 2;
+        int star = 1;
+
+        // Prints each segment of the tree.
+        for (int i = 0; i < segment; i++) {
+
+            // Formulas to allow each segment to grow by one each time.
+            int air = space - i;
+            int leaves = star + (2 * i);
+
+            // Prints each row until height is met.
+            for (int j = 0; j < height; j++) {
+                int starLeaves = leaves + (j * 2);
+                int airSpace = air - j;
+
+                for (int k = 0; k < airSpace; k++) {
+                    IO.print(" ");
+                }
+                for (int k = 0; k < starLeaves; k++) {
+                    IO.print("*");
+                }
+                IO.println();
+            }
+        }
+
+        // Prints tree stand that is centered to the tree length.
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < space; j++) {
+                IO.print(" ");
+            }
+            IO.println("*");
+        }
+        for (int i = 0; i < space - 3; i++) {
+            IO.print(" ");
+        }
+        IO.print("*******");
+    }
 }
 /*
 Write a program that produces images of Christmas trees as output. It should have a method with two parameters:
