dc9d9f252d86e3c2aa19edb47429ccb459204057
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-18 22:14:13 -0800

    Complete Chapter 4 Exercise 1

--- src/Chapter04/Exercises/Exercise01.java
+++ src/Chapter04/Exercises/Exercise01.java
@@ -1,6 +1,17 @@
 package Chapter04.Exercises;
 
 public class Exercise01 {
+    void main() {
+        IO.print(fractionSum(1));
+    }
+
+    public static double fractionSum(int n) {
+        double total = 0;
+        for (int i = 1; i <= n; i++) {
+            total += 1.0 / i;
+        }
+        return total;
+    }
 }
 /*
 Write a method called 'fractionSum' that accepts an integer parameter n and returns as a double the sum of the first n terms of the sequence
