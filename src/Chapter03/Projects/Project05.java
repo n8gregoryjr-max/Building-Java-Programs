0ce9211f36a9ca55f028778592c722eeedea79f7
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-18 20:09:25 -0800

    Complete Chapter 3 Project 6 - Maybe?

--- src/Chapter03/Projects/Project05.java
+++ src/Chapter03/Projects/Project05.java
@@ -1,6 +1,71 @@
 package Chapter03.Projects;
 
+import java.util.Scanner;
+
 public class Project05 {
+    void main() {
+        promptUser(new Scanner(System.in));
+    }
+
+    public static void promptUser(Scanner input) {
+        // First location info.
+        IO.println("What is the latitude and longitude of the first location?");
+        String lat1Dir = input.next();
+        double lat1 = Math.toRadians(input.nextDouble());
+        String lon1Dir = input.next();
+        double lon1 = Math.toRadians(input.nextDouble());
+
+        // Second location info.
+        IO.println("Okay. What is the latitude and longitude of the second location?");
+        String lat2Dir = input.next();
+        double lat2 = Math.toRadians(input.nextDouble());
+        String lon2Dir = input.next();
+        double lon2 = Math.toRadians(input.nextDouble());
+
+        lat1 = latCalc(lat1, lat1Dir);
+        lat2 = latCalc(lat2, lat2Dir);
+        lon1 = lonCalc(lon1, lon1Dir);
+        lon2 = lonCalc(lon2, lon2Dir);
+
+        double lonDiff = lonDiffCalc(lon1, lon2);
+        double partA = Math.sin(lat1) * Math.sin(lat2);
+        double partB = Math.cos(lat1) * Math.cos(lat2) * Math.cos(lonDiff);
+        double angDiff = Math.acos(partA + partB);
+        double r = 6372.795;
+
+        IO.println(r * angDiff + " km");
+
+    }
+
+    public static double latCalc(double lat, String dir) {
+        dir = dir.toLowerCase();
+        if (dir.equals("north")) {
+            return lat;
+        } else if (dir.equals("south")) {
+            return -1 * lat;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static double lonCalc(double lon, String dir) {
+        dir = dir.toLowerCase();
+        if (dir.equals("east")) {
+            return lon;
+        } else if (dir.equals("west")) {
+            return -1 * lon;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static double lonDiffCalc(double a, double b) {
+        if ((a < 0 && b < 0) || (a > 0 && b > 0)) {
+            return Math.abs((a - b));
+        } else {
+            return 360 - (Math.abs(a) + Math.abs(b));
+        }
+    }
 }
 /*
 Write a program that computes the spherical distance between two points on the surface of the Earth, given their latitudes and longitudes.
