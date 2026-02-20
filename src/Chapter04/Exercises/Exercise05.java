e646e32475ead2df45a931c1a7297da8d677705d
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-20 14:02:40 -0800

    Complete Chapter 4 Exercise 5

--- src/Chapter04/Exercises/Exercise05.java
+++ src/Chapter04/Exercises/Exercise05.java
@@ -1,6 +1,21 @@
 package Chapter04.Exercises;
 
 public class Exercise05 {
+   void main(){
+       pow(3, 4);
+   }
+
+    public static void pow(int a, int b) {
+       int A = a;
+       if (b > 0) {
+           for (int i = 1; i < b; i++) {
+                a = a * A;
+           }
+           IO.print(a);
+       } else {
+           IO.print("1");
+       }
+    }
 }
 /*
 Write a method called 'pow' that accepts a base and an exponent as parameters and returns the base raised to the given power.
