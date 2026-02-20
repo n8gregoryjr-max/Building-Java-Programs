5d27781b0e132fc9bf26f69e65d56cef2904ada1
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-19 19:44:05 -0800

    Complete Chapter 4 Exercise 3

--- src/Chapter04/Exercises/Exercise03.java
+++ src/Chapter04/Exercises/Exercise03.java
@@ -1,6 +1,21 @@
 package Chapter04.Exercises;
 
 public class Exercise03 {
+    void main() {
+        IO.print(season(12, 1));
+    }
+
+    public static String season(int a, int b) {
+        if ((a == 12 && b > 15) || a == 1 || a == 2 || (a == 3 && b < 16)) {
+            return "winter";
+        } else if ((a == 3 && b > 15) || a == 4 || a == 5 || (a == 6 && b < 16)) {
+            return "spring";
+        } else if ((a == 6 && b > 15) || a == 7 || a == 8 || (a == 9 && b < 16)) {
+            return "summer";
+        } else {
+            return "fall";
+        }
+    }
 }
 /*
 Write a method called 'season' that takes as parameters two integers representing a month and day and returns a String indicating the season
