c462da8d512e90fb76764acbfe080fc0b508b4e7
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-19 19:30:33 -0800

    Complete Chapter 4 Exercise 2

--- src/Chapter04/Exercises/Exercise02.java
+++ src/Chapter04/Exercises/Exercise02.java
@@ -1,6 +1,21 @@
 package Chapter04.Exercises;
 
 public class Exercise02 {
+    void main() {
+        IO.print(repl("hello", 3));
+    }
+
+    public static String repl(String a, int reps) {
+        String ans = "";
+        if (reps > 0) {
+            for (int i = 0; i < reps; i++) {
+                ans += a;
+            }
+            return ans;
+        } else {
+            return "";
+        }
+    }
 }
 /*
 Write a method called 'repl' that accepts a String and a number of repetitions as parameters and returns the String concatenated that many times.
