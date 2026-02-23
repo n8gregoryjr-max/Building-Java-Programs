dde9c5135da897a4b5dae424f08c38c146fb1acf
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 21:29:03 -0800

    Complete Chapter 4 Exercise 9

--- src/Chapter04/Exercises/Exercise09.java
+++ src/Chapter04/Exercises/Exercise09.java
@@ -1,6 +1,34 @@
 package Chapter04.Exercises;
 
+import java.util.Scanner;
+
 public class Exercise09 {
+    void main() {
+        IO.print("How many integers? ");
+        evenSumMax(new Scanner(System.in));
+    }
+
+    public static void evenSumMax(Scanner input) {
+        // Prompts the user for amount of numbers being input.
+        int reps = input.nextInt();
+
+        // Initialize and define final output values and a placeholder value.
+        int evenMax = 0, evenSum = 0, a;
+
+        for (int i = 0; i < reps; i++) {
+            IO.print("Next integer? ");
+            a = input.nextInt();
+            if (a > evenMax && a % 2 == 0) {
+                evenMax = a;
+            }
+            if (a % 2 == 0) {
+                evenSum += a;
+            }
+        }
+
+
+        IO.print("Even sum = " + evenSum + ", Even max = " + evenMax);
+    }
 }
 /*
 Write a method called 'evenSumMax' that accepts a Scanner for the console as a parameter.
