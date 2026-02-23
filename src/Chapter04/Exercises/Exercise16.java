50b403a4bfaa0a0b1359910393a44e56cab6497e
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 23:11:57 -0800

    Complete Chapter 4 Exercise 16

--- src/Chapter04/Exercises/Exercise16.java
+++ src/Chapter04/Exercises/Exercise16.java
@@ -1,6 +1,32 @@
 package Chapter04.Exercises;
 
+import java.util.Scanner;
+
 public class Exercise16 {
+    void main() {
+        printPalindrome(new Scanner(System.in));
+    }
+
+    public static void printPalindrome(Scanner input) {
+        IO.print("How many words will you be testing? ");
+        int reps = input.nextInt();
+
+        for (int i = 0; i < reps; i++) {
+            IO.print("Enter a word: ");
+            String word = input.next();
+
+            String compare = "";
+            for (int j = 0; j < word.length(); j++) {
+                compare += word.charAt(word.length() - j - 1);
+            }
+
+            if (compare.equals(word)) {
+                IO.println(word + " is a Palindrome!");
+            } else {
+                IO.println(word + " is NOT a Palindrome.");
+            }
+        }
+    }
 }
 /*
 Write a method called 'printPalindrome' that accepts a Scanner for the console as a parameter,
