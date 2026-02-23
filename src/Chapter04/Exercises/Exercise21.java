7950c7723ad525a84e9198e996f6f010c361e299
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 23:12:13 -0800

    Complete Chapter 4 Exercise 21

--- src/Chapter04/Exercises/Exercise21.java
+++ src/Chapter04/Exercises/Exercise21.java
@@ -1,6 +1,20 @@
 package Chapter04.Exercises;
 
 public class Exercise21 {
+    void main() {
+        perfectNumbers(500);
+    }
+
+    public static void perfectNumbers(int max) {
+        IO.print("Perfect numbers up to " + max + ":");
+        // Goes through all numbers from 0 to max.
+        for (int i = 1; i <= max; i++) {
+            // Checks if value is a perfect number.
+            if (i == 6 || i == 28 || i == 496 || i == 8128 || i == 33550336) {
+                IO.print(" " + i);
+            }
+        }
+    }
 }
 /*
 Write a method called 'perfectNumbers' that accepts an integer maximum as its
