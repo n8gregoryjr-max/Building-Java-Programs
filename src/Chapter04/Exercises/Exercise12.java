deb993ea1b57adfdd11333df6c14908fbbe83189
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 22:02:23 -0800

    Complete Chapter 4 Exercise 12

--- src/Chapter04/Exercises/Exercise12.java
+++ src/Chapter04/Exercises/Exercise12.java
@@ -1,6 +1,28 @@
 package Chapter04.Exercises;
 
 public class Exercise12 {
+    void main() {
+        printTriangleType(5, 7, 7);
+        printTriangleType(6, 6, 6);
+        printTriangleType(5, 7, 8);
+        printTriangleType(2, 18, 2);
+    }
+
+    public static void printTriangleType(int a, int b, int c) {
+        // Checks for triangle inequality.
+        if(!(a + b > c && a + c > b && b + c > a)) {
+            throw new IllegalArgumentException();
+        }
+
+        // Checks for type of triangle.
+        if (a == b && b == c) {
+            IO.println("equilateral");
+        } else if (a == b || b == c || a == c) {
+            IO.println("isosceles");
+        } else {
+            IO.println("scalene");
+        }
+    }
 }
 /*
 Write the method called printTriangleType referred to in Self-Check Problem 25.
