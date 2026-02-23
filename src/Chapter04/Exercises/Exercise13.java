391fd1ae811cd76a1350052042d54607be4c2ce1
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 22:28:39 -0800

    Complete Chapter 4 Exercise 13

--- src/Chapter04/Exercises/Exercise13.java
+++ src/Chapter04/Exercises/Exercise13.java
@@ -1,6 +1,13 @@
 package Chapter04.Exercises;
 
 public class Exercise13 {
+    void main() {
+        IO.print(average(100, 201));
+    }
+
+    public static double average(int a, int b) {
+        return (a + b) / 2.0;
+    }
 }
 /*
 Write a method called 'average' that takes two integers as parameters and returns the average of the two integers.
