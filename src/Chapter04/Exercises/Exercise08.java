b1108c1b6925ae3de755d5041c98d89a98f97018
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 21:06:58 -0800

    Complete Chapter 4 Exercise 8

--- src/Chapter04/Exercises/Exercise08.java
+++ src/Chapter04/Exercises/Exercise08.java
@@ -1,6 +1,40 @@
 package Chapter04.Exercises;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercise08 {
+    void main() {
+        IO.print("How many numbers do you want to enter? ");
+        smallestLargest(new Scanner(System.in));
+    }
+
+    public static void smallestLargest(Scanner input) {
+        int reps = input.nextInt();
+        int min = -12, max = 12;
+        int a, smallest = max, largest = min;
+        Random r = new Random();
+
+        for (int i = 1; i <= reps; i++) {
+
+            // Assigns 'a' as a random number.
+            a = r.nextInt(min, max);
+
+            // Prints for each repetition.
+            IO.println("Number " + i + ": " + a);
+
+            // Changes value if 'a' is larger or smaller.
+            if (a < smallest) {
+                smallest = a;
+            }
+            if (a > largest) {
+                largest = a;
+            }
+        }
+        // Finishes by showing the smallest and largest values.
+        IO.println("Smallest = " + smallest);
+        IO.println("Largest = " + largest);
+    }
 }
 /*
 Write a method called 'smallestLargest' that accepts a Scanner for the console as a parameter and asks the user to enter numbers,
