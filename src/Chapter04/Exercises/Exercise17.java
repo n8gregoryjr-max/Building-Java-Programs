591ea5ef9f901ca3e7a89c01b6465eb718541813
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-02-22 23:12:01 -0800

    Complete Chapter 4 Exercise 17

--- src/Chapter04/Exercises/Exercise17.java
+++ src/Chapter04/Exercises/Exercise17.java
@@ -1,6 +1,19 @@
 package Chapter04.Exercises;
 
 public class Exercise17 {
+    void main() {
+        IO.print(stutter("Hello!"));
+    }
+
+    public static String stutter(String word) {
+        String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < 2; j++) {
+                answer += word.charAt(i);
+            }
+        }
+        return answer;
+    }
 }
 /*
 Write a method called 'stutter' that accepts a string parameter and returns that string with its characters repeated twice.
