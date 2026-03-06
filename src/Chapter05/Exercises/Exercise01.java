b25324962b103c709b4e5eee3280b44b6a372cfc
Nathaniel J Gregory <n8gregoryjr@gmail.com>
2026-03-06 00:58:07 -0800

    Complete Chapter 5 Exercise 1

--- src/Chapter05/Exercises/Exercise01.java
+++ src/Chapter05/Exercises/Exercise01.java
@@ -1,6 +1,24 @@
 package Chapter05.Exercises;
 
 public class Exercise01 {
+    void main() {
+        showTwos(7);
+        showTwos(18);
+        showTwos(68);
+        showTwos(120);
+    }
+
+    public static void showTwos(int num) {
+        // Prints initial number.
+        IO.print(num + " = ");
+        // If it is divisible by 2, divides by two until it is not able to.
+        while(num % 2 == 0) {
+            num /= 2;
+            IO.print("2 * ");
+        }
+        // Prints remaining number.
+        IO.println(num);
+    }
 }
 /*
 Write a method called 'showTwos' that shows the factors of 2 in a given integer.
